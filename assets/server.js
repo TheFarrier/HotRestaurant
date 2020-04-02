332d87ce5e003ea3593d276b98ead41ebc468163
The Farrier <nkfarrey@gmail.com>
2020-04-01 20:58:46 -0500

    added verification for full tables to post

--- assets/server.js
+++ assets/server.js
@@ -20,6 +20,18 @@ var tables = [
     email: "skywalker@yahoo.com",
     phone: "900-784-5173",
   },
+  {
+    id: 45615,
+    name: "Obi Wan Kenobi", 
+    email: "Ben@yahoo.com",
+    phone: "900-784-5173",
+  },
+  {
+    id: 456354,
+    name: "Darth Vader", 
+    email: "DaddyVader@yahoo.com",
+    phone: "900-784-5173",
+  },
 ];
 
 var waitlist = [];
@@ -32,42 +44,26 @@ app.get("/api/waitlist", function(req, res) {
   return res.json(waitlist);
 });
 
-function newReservation(){
-  if(tables.length === 5){
-    app.post("/api/waitlist", function(req, res) {
-      // req.body hosts is equal to the JSON post sent from the user
-      // This works because of our body parsing middleware
-      var reservation = req.body;
-    
-      // Using a RegEx Pattern to remove spaces from reservation
-      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-      reservation.routeName = reservation.name.replace(/\s+/g, "").toLowerCase();
-    
-      console.log(reservation);
-    
-      waitlist.push(reservation);
-    
-      res.json(reservation);
-    });
-  } else {
-    app.post("/api/tables", function(req, res) {
-      // req.body hosts is equal to the JSON post sent from the user
-      // This works because of our body parsing middleware
-      var reservation = req.body;
-    
-      // Using a RegEx Pattern to remove spaces from reservation
-      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-      reservation.routeName = reservation.name.replace(/\s+/g, "").toLowerCase();
-    
-      console.log(reservation);
-    
-      tables.push(reservation);
-    
-      res.json(reservation);
-    });
-  }
-};
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var reservation = req.body;
 
+  // Using a RegEx Pattern to remove spaces from reservation
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  reservation.routeName = reservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(reservation);
+
+  if(tables.length === 5){
+    waitlist.push(reservation);
+    console.log("Reservations full. You've been added to the waitlist.")
+  } else {
+    tables.push(reservation);
+  }
+  
+  res.json(reservation);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
